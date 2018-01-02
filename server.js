5774ae819c471d2b77a9a21ddf9d3617387ac973
Andrew Watson <andrewwatson@isoftstone.com>
2018-01-01 17:55:51 -0800

    change server

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-'use strict';
+var path = require('path');
+var express = require('express');
 
-require('express')().use(require('express')
-.static(__dirname + '/build')).listen(process.env.PORT || 8080, ()=> console.log('Client Server Up on 8080'));
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
