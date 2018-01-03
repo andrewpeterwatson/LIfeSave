6fe84e9752220fe0b802152f360c58ccb904e733
Andrew Watson <andrewwatson@isoftstone.com>
2018-01-03 11:17:16 -0800

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname + '/'));
+console.log('Express started on port ', port)
 
-app.listen(process.env.PORT || 8080);
+app.listen(port);
