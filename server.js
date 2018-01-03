6c45f943203b5ad50d50dab8b57b4100d59c5f3c
Andrew Watson <andrewwatson@isoftstone.com>
2018-01-03 09:39:23 -0800

    heroku deployment

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-'use strict';
+const path = require('path')
+const express = require('express')
 
-require('express')().use(require('express')
-.static(__dirname + '/build')).listen(process.env.PORT || 8080, ()=> console.log('Client Server Up on 8080'));
+module.exports = {
+  app: function () {
+    const app = express()
+    const indexPath = path.join(__dirname, '/../index.html')
+    const publicPath = express.static(path.join(__dirname, '../public'))
+
+    app.use('/public', publicPath)
+    app.get('/', function (_, res) { res.sendFile(indexPath) })
+
+    return app
+  }
+}
